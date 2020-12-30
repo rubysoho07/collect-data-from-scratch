ba754ffd8189791ddccd11fc6dfa5e1a7e4c5cce
Yungon Park <hahafree12@gmail.com>
2020-12-30 17:20:13 +0900

    Modify config of Kafka log.dirs & fix script

--- kafka/run-kafka.sh
+++ kafka/run-kafka.sh
@@ -2,13 +2,16 @@
 
 cd kafka_2.13-2.6.0
 
+# Set log file directory
+sed -i 's/log.dirs=\/tmp\/kafka-logs/log.dirs=\/data/' config/server.properties
+
 if [ -n "${STANDALONE_MODE}" ]; then
     # Set ZooKeeper connection
     sed -i 's/zookeeper.connect=localhost:2181/zookeeper.connect=zookeeper:2181/' config/server.properties
 else
-    HOST_ID = $(hostname | cut -d - -f 2)
+    HOST_ID=$(hostname | cut -d - -f 2)
     # Set Broker ID
-    sed -i 's/broker.id=0/broker.id=${HOST_ID}/' config/server.properties
+    sed -i 's/broker.id=0/broker.id='${HOST_ID}'/' config/server.properties
 
     # Set ZooKeeper connection
     sed -i 's/zookeeper.connect=localhost:2181/zookeeper.connect=zookeeper-0.zookeeper.default.svc.cluster.local:2181,zookeeper-1.zookeeper.default.svc.cluster.local:2181,zookeeper-2.zookeeper.default.svc.cluster.local:2181/' config/server.properties
