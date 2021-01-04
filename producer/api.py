75b3df5e3b86c4cd254d8c59f9357a09f0faa6d8
Yungon Park <hahafree12@gmail.com>
2021-01-04 21:22:10 +0900

    Create Kafka Producer for local environment.

---
+++ producer/api.py
@@ -0,0 +1,38 @@
+import os
+import json
+
+from datetime import datetime
+from random import randint
+
+from flask import Flask
+from flask import request
+
+from kafka import KafkaProducer
+
+app = Flask(__name__)
+
+@app.route('/upload', methods=['POST'])
+def upload():
+
+    if request.method == 'POST':
+        if 'STANDALONE_MODE' in os.environ.keys():
+            bootstrap_servers = "kafka:9092"
+        else:
+            bootstrap_servers = [
+                "kafka-0.kafka.default.svc.cluster.local:9092",
+                "kafka-1.kafka.default.svc.cluster.local:9092",
+                "kafka-2.kafka.default.svc.cluster.local:9092" 
+            ]
+        
+        producer = KafkaProducer(bootstrap_servers=bootstrap_servers)
+
+        message = {
+            "message": f"Random Message: {randint(1, 1000)}",
+            "eventTime": datetime.now().isoformat(timespec="milliseconds")
+        }
+
+        producer.send("test-topic", json.dumps(message, ensure_ascii=False).encode())
+
+        return message
+    else:
+        return "Not Available"
